f2eb880dca2409904c60ea00afd5e6ba73ccf79b
cyberspace <cyberspace.dk@gmail.com>
2019-04-16 19:43:18 +0300

    how to upload file

--- multer_send_use_fetch/app.js
+++ multer_send_use_fetch/app.js
@@ -2,13 +2,14 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 
-// ---------------------------------------------------
 const multer = require('multer');
 
 
+// middleware
 app.use(cors());
 app.use(express.json());
 
+// routes
 app.get('/', (req,res)=>{
     res.send('WORK')
 })
