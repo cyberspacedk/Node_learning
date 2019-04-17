12d0911832c8ad9909c39de20dee700311197d97
cyberspace <cyberspace.dk@gmail.com>
2019-04-17 11:13:07 +0300

    multer

--- multer_send_use_fetch/app.js
+++ multer_send_use_fetch/app.js
@@ -1,18 +1,36 @@
 const express = require('express');
+const path = require('path');
 const cors = require('cors');
+const multer = require('multer');
+ 
 const app = express();
 
-const multer = require('multer');
-
+// объект настроек для multer
+const storage = multer.diskStorage({
+    destination: "./public/uploads/",
+    filename: function(req, file, cb){
+       cb(null, `image-${Date.now()}${path.extname(file.originalname)}`);
+    }
+ });
+ 
+ const upload = multer({
+    storage: storage,
+    limits:{fileSize: 10000000},
+ });
 
 // middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json()); 
 
-// routes
+// обрабатываем роут , вызываем upload.single в который передаем значение атрибута name инпута
+app.post("/upload", upload.single("mediafile"), (req, res) => { 
+       res.send(200).json({success: true, message: 'File available on path http://host/upload/filename'});
+    })
+// отдаем картинки. НЕ забыть указывать имя файла
+app.get('/upload', express.static('./public/uploads'));  
+
 app.get('/', (req,res)=>{
     res.send('WORK')
 })
-
-
-app.listen(7000, ()=> console.log('SERVSER START'))
+ 
+app.listen(7000, ()=> console.log('SERVER START on port 7000'))
